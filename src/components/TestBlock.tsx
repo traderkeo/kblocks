6317a663a99e49e3ffec2835f488c3340e18b67c
Keon Armin <keona@nonamesecurity.com>
2023-12-25 00:03:42 -0800

    update

---
+++ src/components/TestBlock.tsx
@@ -0,0 +1,8 @@
+
+export default function TestBlock() {
+    return (
+      <div>
+        <h1>TestBlock</h1>
+      </div>
+    );
+  }
--- src/components/TestBlock/index.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function TestBlock() {
-  return (
-    <div>
-      <h1>TestBlock</h1>
-    </div>
-  );
-}
